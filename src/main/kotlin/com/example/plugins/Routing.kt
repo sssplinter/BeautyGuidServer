3d57a57a3cd073761125747dd5ba35f6b8038209
Kristina Sementsova <kristina.sementsova@intexsoft.by>
2023-01-04 12:19:48 +0300

    create data base stub

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,12 +3,23 @@ package com.example.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Test(
+    val text: String
+)
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respond(
+                listOf(
+                    Test(text = "- Hello, my server"),
+                    Test(text = "- Hello, my owner")
+                )
+            )
         }
     }
 }
