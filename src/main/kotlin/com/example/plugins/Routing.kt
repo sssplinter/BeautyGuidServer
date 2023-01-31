ed0340428cc9b62e7bb4461544c8e8f15019bb14
Kristina <kristinasementsova21@gmaol.com>
2023-02-01 00:23:14 +0300

    add routing articles

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -10,36 +10,38 @@ import io.ktor.server.util.*
 
 fun Application.configureRouting() {
     routing {
-        get {
-            call.respond(ArticlesResponseRemote(dao.allArticles()))
-        }
-        post {
-            val newArticleReq = call.receive<NewArticle>()
-            val article = dao.addNewArticle(newArticleReq.title, newArticleReq.body)
-            call.respondRedirect("/${article?.id}")
-        }
-        get("{id}") {
-            val id = call.parameters.getOrFail<Int>("id").toInt()
-            call.respond(ArticlesResponseRemote(listOf(dao.article(id))))
-        }
-        get("{id}/edit") {
-            val id = call.parameters.getOrFail<Int>("id").toInt()
-            call.respond(ArticlesResponseRemote(listOf(dao.article(id))))
-        }
-        post("{id}") {
-            val id = call.parameters.getOrFail<Int>("id").toInt()
-            val formParameters = call.receiveParameters()
-            when (formParameters.getOrFail("_action")) {
-                "update" -> {
-                    val title = formParameters.getOrFail("title")
-                    val body = formParameters.getOrFail("body")
-                    dao.editArticle(id, title, body)
-                    call.respondRedirect("/articles/$id")
-                }
+        route("articles") {
+            get {
+                call.respond(ArticlesResponseRemote(dao.allArticles()))
+            }
+            post {
+                val newArticleReq = call.receive<NewArticle>()
+                val article = dao.addNewArticle(newArticleReq.title, newArticleReq.body)
+                call.respondRedirect("/articles/${article?.id}")
+            }
+            get("{id}") {
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(ArticlesResponseRemote(listOf(dao.article(id))))
+            }
+            get("{id}/edit") {
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                call.respond(ArticlesResponseRemote(listOf(dao.article(id))))
+            }
+            post("{id}") {
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                val formParameters = call.receiveParameters()
+                when (formParameters.getOrFail("_action")) {
+                    "update" -> {
+                        val title = formParameters.getOrFail("title")
+                        val body = formParameters.getOrFail("body")
+                        dao.editArticle(id, title, body)
+                        call.respondRedirect("/articles/$id")
+                    }
 
-                "delete" -> {
-                    dao.deleteArticle(id)
-                    call.respondRedirect("")
+                    "delete" -> {
+                        dao.deleteArticle(id)
+                        call.respondRedirect("/articles")
+                    }
                 }
             }
         }
