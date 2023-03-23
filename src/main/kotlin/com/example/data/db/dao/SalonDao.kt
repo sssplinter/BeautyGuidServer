db3df623a9a0ccd8f50b3dec686c625c2448a23c
sssplinter <kristinasementsova21@gmail.com>
2023-03-23 15:43:00 +0300

    implement salon DAO

---
+++ src/main/kotlin/com/example/data/db/dao/SalonDao.kt
@@ -0,0 +1,42 @@
+package com.example.data.db.dao
+
+import com.example.data.db.DatabaseFactory
+import com.example.data.db.table.CategoryTable
+import com.example.data.db.table.SalonM2MCategory
+import com.example.data.db.table.SalonTable
+import com.example.data.models.Category
+import com.example.data.models.Salon
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+
+class SalonDao {
+    private fun resultRowToSalon(row: ResultRow) = Salon(
+        id = row[SalonTable.id].value,
+        photoUrl = row[SalonTable.photoUrl],
+        name = row[SalonTable.name],
+        description = row[SalonTable.description],
+        rating = row[SalonTable.rating]
+    )
+
+    private fun resultRowToCategory(row: ResultRow) = Category(
+        id = row[CategoryTable.id].value,
+        name = row[CategoryTable.name]
+    )
+
+    suspend fun getAllSalons(): List<Salon> = DatabaseFactory.dbQuery {
+        SalonTable.selectAll().map(::resultRowToSalon)
+    }
+
+    suspend fun getSalonCategories(salonId: Int): List<String> = DatabaseFactory.dbQuery {
+        val categories =
+            (SalonTable innerJoin SalonM2MCategory innerJoin CategoryTable).select(SalonTable.id eq salonId)
+                .map(::resultRowToCategory)
+        categories.map { it.name }
+    }
+
+    suspend fun fetchSalonById(salonId: Int): Salon? = DatabaseFactory.dbQuery {
+        SalonTable.select(SalonTable.id eq salonId).map(::resultRowToSalon).singleOrNull()
+    }
+}
